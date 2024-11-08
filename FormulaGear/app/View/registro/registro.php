3663594b6ac36ea67a3803e3bcda5d54d90487f8
Mathias <mveirad@gmail.com>
2024-11-08 13:56:58 +0100

    filtros añadidos en registro

--- FormulaGear/app/View/registro/registro.php
+++ FormulaGear/app/View/registro/registro.php
@@ -1,18 +1,20 @@
 <?php
 require_once '../../Controller/UsuarioController.php';
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['terminos-condiciones'])) {
     $nombreFiltrado = htmlspecialchars($_POST['nombre']);
     $correoFiltrado = htmlspecialchars($_POST['email']);
     $passFiltrada = htmlspecialchars($_POST['password']);
-    if($nombreFiltrado && $correoFiltrado && $passFiltrada){
+
+    if (filter_var($correoFiltrado, FILTER_VALIDATE_EMAIL)) {
         $userController = new UsuarioController();
-        if($userController->crearUsuario($nombreFiltrado,$correoFiltrado,$passFiltrada)){
-            echo "Usuario creado";
-        }else{
+        if ($userController->crearUsuario($nombreFiltrado, $correoFiltrado, $passFiltrada)) {
+            header("Location: /FormulaGear/FormulaGear/app/View/main/main.html");
+        } else {
             echo "Correo en uso";
         }
-    }else{
+    } else {
         echo "Input no válido";
     }
+}else{
+    echo "va a ser que no";
 }
-

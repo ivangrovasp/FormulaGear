ecf228459642c20b096bcca5e3a3b8209a8d7c43
Mathias <mveirad@gmail.com>
2024-11-12 12:26:28 +0100

    agregar datos al script sql

--- Documentacion/script.sql
+++ Documentacion/script.sql
@@ -1,14 +1,18 @@
 /*
 INSERTAR USUARIOS
 */
-
-INSERT INTO `usuario`( `nombreUsuario`, `correoUsuario`, `permisosUsuario`, `passUsuario`) VALUES ('Mathias','mathiasveira@gmail.com',0,'mathias123');
 INSERT INTO `usuario`( `nombreUsuario`, `correoUsuario`, `permisosUsuario`, `passUsuario`) VALUES ('Admin','admin@gmail.com',1,'admin123');
+INSERT INTO `usuario`( `nombreUsuario`, `correoUsuario`, `permisosUsuario`, `passUsuario`) VALUES ('Mathias','mathiasveira@gmail.com',0,'mathias123');
+INSERT INTO `usuario`( `nombreUsuario`, `correoUsuario`, `permisosUsuario`, `passUsuario`) VALUES ('Ivan','ivan@gmail.com',0,'ivan123');
+INSERT INTO `usuario`( `nombreUsuario`, `correoUsuario`, `permisosUsuario`, `passUsuario`) VALUES ('Alberto','alberto@gmail.com',0,'alberto123');
 /*
 INSERTAR PRODUCTOS
 */
-INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Camiseta Ferrari',50,'Camiseta de ferrari chida','C:/formulagear_img/camiseta-ferrari.jpg',0);
-INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Camiseta Mercedes',50,'Camiseta de Mercedes chida','C:/formulagear_img/camiseta-mercedes.avif',0);
+INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Camiseta Ferrari',50,'Consigue el estilo de tu equipo favorito de carreras. Este artículo icónico añade un toque de velocidad a cualquier otra prenda para que puedas mostrar tus simpatías en la pista y en cualquier otra parte.','C:\xampp/htdocs/FormulaGear/FormulaGear/imagenes/camiseta-ferrari.jpg',60);
+INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Camiseta Mercedes',40,'Consigue el estilo de tu equipo favorito de carreras. Este artículo icónico añade un toque de velocidad a cualquier otra prenda para que puedas mostrar tus simpatías en la pista y en cualquier otra parte.','C:\xampp/htdocs/FormulaGear/FormulaGear/imagenes/mercedesCamiseta.png',50);
+INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Camiseta Aston Martin',70,'Consigue el estilo de tu equipo favorito de carreras. Este artículo icónico añade un toque de velocidad a cualquier otra prenda para que puedas mostrar tus simpatías en la pista y en cualquier otra parte.','C:\xampp/htdocs/FormulaGear/FormulaGear/imagenes/camisetaAstonMartin.png',40);
+INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Gorra Red Bull',20,'Consigue el estilo de tu equipo favorito de carreras. Este artículo icónico añade un toque de velocidad a cualquier otra prenda para que puedas mostrar tus simpatías en la pista y en cualquier otra parte.','C:\xampp/htdocs/FormulaGear/FormulaGear/imagenes/gorra-red-bull.webp',5);
+INSERT INTO `producto`(`nombreProducto`,`precioProducto`,`descripcionProducto`,`imagenProducto`,`numeroLikesProducto`) VALUES ('Coche Miniatura Fernando Alonso',100,'Consigue el estilo de tu equipo favorito de carreras. Este artículo icónico añade un toque de velocidad a cualquier otra prenda para que puedas mostrar tus simpatías en la pista y en cualquier otra parte.','C:\xampp/htdocs/FormulaGear/FormulaGear/imagenes/gorra-red-bull.webp',70);
 /*
 INSERTAR TALLA
 */
@@ -16,11 +20,16 @@ INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('XL',1);
 INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('L',1);
 INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('XL',2);
 INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('L',2);
+INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('XL',3);
+INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('L',3);
+INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('XL',4);
+INSERT INTO `talla`(`nombreTalla`,`idProducto`)VALUES('L',4);
 /*
 INSERTAR PRODUCTOS FAVORITOS
 */
 INSERT INTO `favorito`(`idUsuario`,`idProducto`)VALUES(1,1);
 INSERT INTO `favorito`(`idUsuario`,`idProducto`)VALUES(2,2);
 /*INSERTAR PEDIDO*/
-INSERT INTO `pedido`(`idUsuario`,`idProducto`)VALUES(1,1);
-INSERT INTO `pedido`(`idUsuario`,`idProducto`)VALUES(2,2);
+INSERT INTO `pedido`(`idUsuario`,`idProducto`)VALUES(2,5);
+INSERT INTO `pedido`(`idUsuario`,`idProducto`)VALUES(3,1);
+INSERT INTO `pedido`(`idUsuario`,`idProducto`)VALUES(4,2);

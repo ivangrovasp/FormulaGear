48ad99d1b9418048a2cdff9d68411ff2db43f2b6
Albertotv <albertotv.aulanosa.com>
2024-11-25 10:43:25 +0100

    arreglamos otra vez los estilos

--- FormulaGear/app/View/favoritos/favorito.php
+++ FormulaGear/app/View/favoritos/favorito.php
@@ -19,83 +19,86 @@ $products = $sesion->obtenerVariableSesion("favoritos");
 </head>
 
 <body>
-    <header class="header-container">
+    <div class="container">
+        <header class="header-container">
 
-        <div class="logo-container">
-            FormulaGear
-        </div>
+            <div class="logo-container">
+                FormulaGear
+            </div>
 
-        <div class="img-container">
-            <img id="neumatico" src="../../../imagenes/neumatico.png" alt="Neumático f1">
-        </div>
-        <nav>
-            <ul class="nav-links">
-                <li><a href="../favoritos/favorito.php">Favoritos</a></li>
-                <li><a href="../productos/productos.php">Productos</a></li>
-                <li><a href="../perfil/perfil.php">Perfil</a></li>
-                <li><a href="../main/main.php">Inicio</a></li>
-                <div class="perfil-image">
-                    <a href="../perfil/perfil.php">
-                        <img id="persona-image" src="../../../Imagenes/perfil.png">
-                        <p><?= $user['nombreUsuario'] ?></p>
-                    </a>
-                </div>
+            <div class="img-container">
+                <img id="neumatico" src="../../../imagenes/neumatico.png" alt="Neumático f1">
+            </div>
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="../favoritos/favorito.php">Favoritos</a></li>
+                    <li><a href="../productos/productos.php">Productos</a></li>
+                    <li><a href="../perfil/perfil.php">Perfil</a></li>
+                    <li><a href="../main/main.php">Inicio</a></li>
+                    <div class="perfil-image">
+                        <a href="../perfil/perfil.php">
+                            <img id="persona-image" src="../../../Imagenes/perfil.png">
+                            <p><?= $user['nombreUsuario'] ?></p>
+                        </a>
+                    </div>
 
-            </ul>
-        </nav>
-    </header>
-    <div class="trending-products">
-        <?php
-        if ($products) {
-            for ($i = 0; $i < count($products); $i++) {
-                $product_id = $products[$i]['idProducto'];
-        ?>
-                <a href="../detailProduct/detail.php?id=<?= $product_id ?>" class="contenido-container">
-                    <div class="detail-header">
-                        <img class="img-product" src="<?= $products[$i]['imagenProducto'] ?>">
-                        <div class="likes">
-                            <p><?= $products[$i]['numeroLikesProducto'] ?></p>
-                            <img class="img-like" src="../../../Imagenes/corazon.png">
+                </ul>
+            </nav>
+        </header>
+        <div class="trending-products">
+            <?php
+            if ($products) {
+                for ($i = 0; $i < count($products); $i++) {
+                    $product_id = $products[$i]['idProducto'];
+            ?>
+                    <a href="../detailProduct/detail.php?id=<?= $product_id ?>" class="contenido-container">
+                        <div class="detail-header">
+                            <img class="img-product" src="<?= $products[$i]['imagenProducto'] ?>">
+                            <div class="likes">
+                                <p><?= $products[$i]['numeroLikesProducto'] ?></p>
+                                <img class="img-like" src="../../../Imagenes/corazon.png">
+                            </div>
                         </div>
-                    </div>
-                    <div class="detail-container">
-                        <p><?= $products[$i]['nombreProducto'] ?></p>
-                        <p class="precio"><?= $products[$i]['precioProducto'] ?>€</p>
-                    </div>
-                </a>
-        <?php
-            }
-        }
-        ?>
-    </div>
-    <div id="botonComprar">
-        <form method="post">
-            <input type="hidden" name="guardar">
-            <button type="submit" name="guardar">
-                Guardar Lista
-                <?php
-                if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['guardar'])) {
-                    if ($products) {
-                        for ($i = 0; $i < count($products); $i++) {
-                            $product_id = $products[$i]['idProducto'];
-                            $favoritoController->favoritoUserId($user['idUsuario'], $product_id);
-                        } 
-                        unset($_SESSION['favoritos']);
-                        header("Location: favorito.php");
-                        exit();
-                    }
+                        <div class="detail-container">
+                            <p><?= $products[$i]['nombreProducto'] ?></p>
+                            <p class="precio"><?= $products[$i]['precioProducto'] ?>€</p>
+                        </div>
+                    </a>
+            <?php
                 }
-                ?>
-            </button>
-        </form>
-    </div>
-    <div class="footer">
-        <div class="footer-content">
-            <p class="pfooter"><img class="rrss" src="../../../Imagenes/gmail.png">FormulaGear@gmail.com</p>
-            <p class="pfooter"><img class="rrss" src="../../../Imagenes/twitter.png">FormulaGear</p>
-            <p class="pfooter"><img class="rrss" src="../../../Imagenes/instagram.png">FormulaGear</p>
+            }
+            ?>
+        </div>
+        <div id="botonComprar">
+            <form method="post">
+                <input type="hidden" name="guardar">
+                <button type="submit" name="guardar">
+                    Guardar Lista
+                    <?php
+                    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['guardar'])) {
+                        if ($products) {
+                            for ($i = 0; $i < count($products); $i++) {
+                                $product_id = $products[$i]['idProducto'];
+                                $favoritoController->favoritoUserId($user['idUsuario'], $product_id);
+                            }
+                            unset($_SESSION['favoritos']);
+                            header("Location: favorito.php");
+                            exit();
+                        }
+                    }
+                    ?>
+                </button>
+            </form>
+        </div>
+        <div class="footer">
+            <div class="footer-content">
+                <p class="pfooter"><img class="rrss" src="../../../Imagenes/gmail.png">FormulaGear@gmail.com</p>
+                <p class="pfooter"><img class="rrss" src="../../../Imagenes/twitter.png">FormulaGear</p>
+                <p class="pfooter"><img class="rrss" src="../../../Imagenes/instagram.png">FormulaGear</p>
+            </div>
         </div>
     </div>
+
 
 </body>
 

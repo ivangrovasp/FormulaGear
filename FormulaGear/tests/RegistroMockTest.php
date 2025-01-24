d379d24eb989ec0c1244485680ab2de7c64f0484
Mathias <mveirad@gmail.com>
2025-01-24 12:39:58 +0100

    mock test registro

--- FormulaGear/tests/RegistroMockTest.php
+++ FormulaGear/tests/RegistroMockTest.php
@@ -3,9 +3,14 @@
 use PHPUnit\Framework\TestCase;
 require_once "../../Controller/UsuarioController.php";
 require_once "../../Model/Usuario.php";
+require_once "../../Model/Sesion.php";
 Class RegistroMockTest extends TestCase{
     public function testRegistroMock(){
         $mockRegister = $this->createMock(Usuario::class);
+        $mockRegister->method('getUser')->willReturn([new Usuario(0,'Mathias28','mathiasveira@gmail.com',false,'mathias123')]);
+        $sesion = new Sesion();
+        $sesion->iniciarVariableSesion("usuario",$mockRegister);
+        $this->assertNotEmpty($sesion->obtenerVariableSesion("usuario"));
     }
 
 }
